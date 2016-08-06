5e70f1f3580a9f0a79b106f8a45dd58cd09e77ff
taoyanqi <yqtao@whu.edu.cn>
2016-08-06 16:36:19 +0800

    reverse integer

--- LeetCodeSolutions/ReverseInteger/ReverseInteger.cpp
+++ LeetCodeSolutions/ReverseInteger/ReverseInteger.cpp
@@ -1,50 +1,37 @@
-//Author: yqtao
-//Email : yqtao@whu.edu.cn
-//Date  :2016.7.5
-/**********************************************************************************
-*
+// Date   : 2016.08.06
+// Author : yqtao
+// https://github.com/yqtaowhu
+
+/********************************************************************************** 
+* 
 * Reverse digits of an integer.
-*
+* 
 * Example1: x =  123, return  321
 * Example2: x = -123, return -321
-*
-*first ,we should think the number  over/underflow,it is very importent;
-*then ,we need not to think the number is a negtive,like -123,it do not like string to integer
-*so we can easy to deal the problems,
+* 
+* 
+* Have you thought about this?
+* 
+* Here are some good questions to ask before coding. Bonus points for you if you have already thought through this!
+* 
+* > If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+* 
+* > Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, 
+*   then the reverse of 1000000003 overflows. How should you handle such cases?
+* 
+* > Throw an exception? Good, but what if throwing an exception is not an option? 
+*   You would then have to re-design the function (ie, add an extra parameter).
+* 
+*               
 **********************************************************************************/
-#include<iostream>
-#include <vector>
-using namespace std;
-int reverse(int x) {
-	int y = 0;
-	int n;
-	while (x != 0) {
-		n = x % 10;
-		//Checking the over/underflow.
-		//Actually, it should be y>(INT_MAX-n)/10, but n/10 is 0, so omit it.
-		if (y > INT_MAX / 10 || y < INT_MIN / 10) {
-			return 0;
-		}
-		y = y * 10 + n;
-		x /= 10;
-	}
-	return y;
-}
-//Actually,we can using long long is a 64bit numbers ,it cant to over/underflow
-//for example,it is so smart;
-int reverse1(int x) {
-	long long res = 0;
-	while (x) {
-		res = res * 10 + x % 10;
-		x /= 10;
-	}
-	return (res<INT_MIN || res>INT_MAX) ? 0 : res;
-}
-int main()
-{
-	int x1 =-123,x2 = 1534236469,x3 = 4,x4 = -12340;
-	cout << reverse1(x1) << endl;
-	cout << reverse1(x2) << endl;
-	cout << reverse1(x3) << endl;
-	cout << reverse1(x4) << endl;
-}
+class Solution {
+public:
+    int reverse(int x) {
+        long long res = 0;
+        while(x) {
+            res = res*10 + x%10;
+            x /= 10;
+        }
+        return (res<INT_MIN || res>INT_MAX) ? 0 : res;
+    }
+};
