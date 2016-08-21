97bf193ce5b1c4ec9933055c0acc5bec5bb46d29
taoyanqi <yqtao@whu.edu.cn>
2016-08-21 17:52:35 +0800

    revise

--- LeetCodeSolutions/LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
+++ LeetCodeSolutions/LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
@@ -57,3 +57,39 @@ string longestPalindrome(string s) {
   
   return s.substr((centerIndex - 1 - maxLen)/2, maxLen);
 }
+//another way ,it may slower than above,but it can easy to understand
+#include<iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+string longestPalindrome(string s) {
+    if (s.empty()) return "";
+    if (s.size() == 1) return s;
+    int min_start = 0, max_len = 1;      //
+    for (int i = 0; i < s.size();) {
+        if (s.size() - i <= max_len / 2) break;
+        int j = i, k = i;       //j is a pre pointer,k is next pointer
+        while (k < s.size() - 1 && s[k + 1] == s[k])
+            ++k; // Skip duplicate characters.
+        i = k + 1;
+        while (k < s.size() - 1 && j > 0 && s[k + 1] == s[j - 1]) 
+        {
+            ++k; 
+            --j; 
+        } // Expand.
+        int new_len = k - j + 1;
+        if (new_len > max_len) 
+        {
+            min_start = j;
+            max_len = new_len; 
+        }
+    }
+    return s.substr(min_start, max_len);
+}
+//测试
+int main()
+{
+    string s = "abadcacd";
+    string result = longestPalindrome(s);
+    cout << result << endl;
+}
