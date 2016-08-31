1ac78c36e6d88f28c0ae3f0945457175da7f5899
taoyanqi <yqtao@whu.edu.cn>
2016-08-31 15:06:25 +0800

    permutations

---
+++ LeetCodeSolutions/permutations/permutations.cpp
@@ -0,0 +1,82 @@
+// reference: https://oj.leetcode.com/problems/permutations/
+
+
+/********************************************************************************** 
+* 
+* Given a collection of numbers, return all possible permutations.
+* 
+* For example,
+* [1,2,3] have the following permutations:
+* [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+* 
+*               
+**********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+{ 1 2 3 }
+{ 2 1 3 }
+{ 3 2 1 }
+{ 1 3 2 }
+{ 2 3 1 }
+{ 3 1 2 }
+*/
+
+/*
+ *    The algroithm - Take each element in array to the first place.
+ *
+ *    For example: 
+ *    
+ *         0) initalization 
+ * 
+ *             pos = 0
+ *             [1, 2, 3]   
+ *
+ *         1) take each element into the first place, 
+ *
+ *             pos = 1
+ *             [1, 2, 3]  ==>  [2, 1, 3] , [3, 1, 2] 
+ *
+ *             then we have total 3 answers
+ *             [1, 2, 3],  [2, 1, 3] , [3, 1, 2] 
+ *            
+ *         2) take each element into the "first place" -- pos 
+ *
+ *             pos = 2
+ *             [1, 2, 3]  ==>  [1, 3, 2]
+ *             [2, 1, 3]  ==>  [2, 3, 1]
+ *             [3, 1, 2]  ==>  [3, 2, 1] 
+ *
+ *             then we have total 6 answers
+ *             [1, 2, 3],  [2, 1, 3] , [3, 1, 2], [1, 3, 2], [2, 3, 1], [3, 2, 1]
+ *
+ *          3) pos = 3 which greater than length of array, return.
+ *
+ */
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+	vector<vector<int> > vv;
+	vv.push_back(num);
+	if (num.size() <2) 
+		return vv;
+	int pos = 0;
+	while (pos<num.size() - 1) {
+		int size = vv.size();
+		for (int i = 0; i<size; i++) {
+			for (int j = pos + 1; j<vv[i].size(); j++) {
+				vector<int> v = vv[i];
+			    swap(v[j],v[pos]);
+				vv.push_back(v);
+			}
+		}
+		pos++;
+	}
+	return vv;
+}
+};
