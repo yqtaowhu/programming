61a28c757fc7a5d85120b69c1c42b0462172d65a
taoyanqi <yqtao@whu.edu.cn>
2016-09-03 15:25:42 +0800

    merge two sorted lists

--- LeetCodeSolutions/MergeTwoSortedLists/MergeTwoSortedLists.cpp
+++ LeetCodeSolutions/MergeTwoSortedLists/MergeTwoSortedLists.cpp
@@ -16,28 +16,23 @@
 *     ListNode(int x) : val(x), next(NULL) {}
 * };
 */
-//
-//Dummy is there to store a pointer to the new head of the list.Tail starts off as what will be the head, 
-//but it changes, so dummy stores the pointer to the head.
 class Solution {
 public:
-	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-		ListNode dummy(INT_MIN);
-		ListNode *tail = &dummy;
-
-		while (l1 && l2) {
-			if (l1->val < l2->val) {
-				tail->next = l1;
-				l1 = l1->next;
-			}
-			else {
-				tail->next = l2;
-				l2 = l2->next;
-			}
-			tail = tail->next;
-		}
-
-		tail->next = l1 ? l1 : l2;
-		return dummy.next;
-	}
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode preHead(0);
+        ListNode* pre=&preHead;
+        while (l1&&l2) {
+            if (l1->val<l2->val) {
+                pre->next=l1;
+                l1=l1->next;
+            }
+            else {
+                pre->next=l2;
+                l2=l2->next;
+            }
+            pre=pre->next;
+        }
+        pre->next=l1 ? l1 : l2;
+        return preHead.next;
+    }
 };
