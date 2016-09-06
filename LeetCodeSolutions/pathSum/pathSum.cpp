e3e594a20b8a047de4e7cdf3c7fc75084a5f93f1
taoyanqi <yqtao@whu.edu.cn>
2016-09-06 16:16:17 +0800

    path sum

--- LeetCodeSolutions/pathSum/pathSum.cpp
+++ LeetCodeSolutions/pathSum/pathSum.cpp
@@ -33,35 +33,37 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ //迭代
 class Solution {
 public:
-	bool hasPathSum1(TreeNode *root, int sum) {
+     bool hasPathSum(TreeNode *root, int sum) {
+        if (root==NULL) return false;
+        vector<TreeNode*> v;
+        v.push_back(root);
+        while(v.size()>0){
+            TreeNode* node = v.back();
+            v.pop_back();
+            if (node->left==NULL && node->right==NULL){
+                if (node->val == sum){
+                    return true;
+                }
+            }
+            if (node->left){
+                node->left->val += node->val;
+                v.push_back(node->left);
+            }
+            if (node->right){
+                node->right->val += node->val;
+                v.push_back(node->right);
+            }
+        }
+        return false;
+        
+    }
+};
+//递归
+bool hasPathSum(TreeNode *root, int sum) {
         if (root == NULL) return false;
         if (root->val == sum && root->left ==  NULL && root->right == NULL) return true;
         return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
     }
-    
-   bool  hasPathSum2(TreeNode *root, int sum){
-    if(!n) return false;
-    stack<TreeNode *> st;
-    TreeNode *cur = root, *pre;
-    while(cur || !st.empty()){
-        while(cur){
-            st.push(cur);
-            sum -= cur->val;
-            cur = cur->left;
-        }
-        cur = st.top();
-        if(!cur->left && !cur->right && !sum) return true;
-        if(cur->right && pre != cur->right) cur = cur->right;
-        else{
-            st.pop();
-            sum += cur->val;
-            pre = cur;
-            cur = NULL;
-        }
-    }
-    return false;
-	}
-    
-};
