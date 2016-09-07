76c518c6eb841c96d0adda51be829c27930abd14
taoyanqi <yqtao@whu.edu.cn>
2016-09-07 16:47:56 +0800

    revise sudoku

--- LeetCodeSolutions/validSudoku/validSudoku.cpp
+++ LeetCodeSolutions/validSudoku/validSudoku.cpp
@@ -16,26 +16,19 @@
 * 
 *               
 **********************************************************************************/
-Three flags are used to check whether a number appear.
-used1: check each row
-used2: check each column
-used3: check each sub-boxes
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char> > &board)
-    {
-        int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
-        
-        for(int i = 0; i < board.size(); ++ i)
-            for(int j = 0; j < board[i].size(); ++ j)
-                if(board[i][j] != '.')
-                {
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool row[9][9] = {false}, col[9][9] = {false}, box[9][9] = {false};
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
                     int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
-                    if(used1[i][num] || used2[j][num] || used3[k][num])
-                        return false;
-                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
+                    if (row[i][num] || col[j][num] || box[k][num]) return false;
+                    row[i][num] = col[j][num] = box[k][num] = true;
                 }
-        
+            }
+        }
         return true;
     }
-};
+}; 
