43bb72a936b359df907fa4ba49a059059d8ab9d2
taoyanqi <yqtao@whu.edu.cn>
2016-09-20 19:41:16 +0800

    revise

--- LeetCodeSolutions/populatingNextRightPointersInEachNode/populatingNextRightPointersInEachNode.cpp
+++ LeetCodeSolutions/populatingNextRightPointersInEachNode/populatingNextRightPointersInEachNode.cpp
@@ -41,28 +41,28 @@
 * 
 *               
 **********************************************************************************/
-
-#include <stdio.h>
-#include <vector>
-#include <queue>
-using namespace std;
-
 /**
  * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
  */
- void connect(TreeLinkNode *root) {
-    if(!root)  return;
-    while(root -> left)
-    {
-        TreeLinkNode *p = root;
-        while(p)
-        {
-            p -> left -> next = p -> right;
-            if(p -> next)
-                p -> right -> next = p -> next -> left;
-            p = p -> next;
+class Solution {
+public:
+     void connect(TreeLinkNode *root) {
+        if(!root)  return;
+        while(root -> left) {
+            TreeLinkNode *p = root;
+            while(p) {
+                p -> left -> next = p -> right;
+                if(p -> next)
+                    p -> right -> next = p -> next -> left;
+                p = p -> next;
+            }
+            root = root -> left;
         }
-        root = root -> left;
-    }
-}
+     }
+};
 
