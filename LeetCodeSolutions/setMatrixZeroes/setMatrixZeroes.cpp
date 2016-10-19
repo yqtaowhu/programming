e770ca6a1082b5499b9f40789c84af28658f8af1
taoyanqi <yqtao@whu.edu.cn>
2016-10-19 21:06:17 +0800

    add another solution

--- LeetCodeSolutions/setMatrixZeroes/setMatrixZeroes.cpp
+++ LeetCodeSolutions/setMatrixZeroes/setMatrixZeroes.cpp
@@ -38,6 +38,29 @@ public:
         }
     }
 };
+//very easy to understand
+//O(N)
+class Solution {
+public:
+    void setZeroes(vector<vector<int> > &matrix) {
+    int rows = matrix.size(), cols = matrix[0].size();
+    vector<int> r(rows,0),c(cols,0);
+    for (int i=0;i<rows;i++) {
+        for (int j=0;j<cols;j++) {
+            if (matrix[i][j]==0) {
+                r[i]=1;
+                c[j]=1;
+            }
+        }
+    }
+    for (int i=0;i<rows;i++) {
+        for (int j=0;j<cols;j++) {
+            if (r[i]||c[j])
+                matrix[i][j]=0;
+        }
+    }
+}
+};
 //another way O(1) SPACE
 class Solution {
 public:
