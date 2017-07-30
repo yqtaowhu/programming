bc75b9f817fdb8d807241b9fdcb39e66c68c8095
taoyanqiwhu <yqtao@whu.edu.cn>
2017-07-30 14:41:29 +0800

    add inorderd travesal next node

--- practice/tree/inorder_next_node.cpp
+++ practice/tree/inorder_next_node.cpp
@@ -1,17 +1,19 @@
+
 /****************************************************************************
-给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
-注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
+find next node of inordertravel 
 ********************************************************************************/
 
 TreeLinkNode* GetNext(TreeLinkNode* pNode) {
-         TreeLinkNode *p=pNode;
-        if (pNode->right) {               //当前节点有右子树
+       TreeLinkNode *p=pNode;
+	   //if node have right node
+       if (pNode->right) {              
             p=pNode->right;
             while (p->left) p=p->left;
             return p;
         }
-        else {                            //没有右子树，向上查找
+        else {                           
 			p=pNode->next;
+			//remember conditional
             while (p) {
              	if (p->left==pNode) return p;  
                 else {
